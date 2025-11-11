a28e905ddbcbbc7a2f0318533069f652b0d9040a
Egor Baranov <egor.baranov@jetbrains.com>
2025-11-11 14:23:51 +0100

    Initial Commit

---
+++ spotify-liquid-glass/spotify_liquid_glassApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_liquid_glassApp.swift
+//  spotify-liquid-glass
+//
+//  Created by Egor Baranov on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct spotify_liquid_glassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
